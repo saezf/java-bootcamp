0f31432baebe6a993441befaff9805f36aadcd45
Christian <dbchdb@gmail.com>
2015-01-10 16:37:24 -0300

    mend

---
+++ t0e1/src/t0e1/s1/i1/Circle.java
@@ -0,0 +1,55 @@
+package t0e1.s1.i1;
+
+public class Circle { // save as "Circle.java"
+	// private instance variable, not accessible from outside this class
+	private double radius;
+	private String color;
+
+	// 1st constructor, which sets both radius and color to default
+	public Circle() {
+		radius = 1.0;
+		color = "red";
+	}
+
+	// 2nd constructor with given radius, but color default
+	public Circle(double radius) {
+		this.radius = radius;
+		this.color = "red";
+	}
+
+	// A public method for retrieving the radius
+	public double getRadius() {
+		return radius;
+	}
+
+	// A public method for computing the area of circle
+	public double getArea() {
+		return radius * radius * Math.PI;
+	}
+
+	// Constructor to construct a new instance of Circle with the given radius
+	// and color
+	public Circle(double radius, String color) {
+		this.radius = radius;
+		this.color = color;
+	}
+
+	// Getter for instance variable color
+	public String getColor() {
+		return this.color;
+	}
+
+	// Setter for instance variable radius
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	// Setter for instance variable color
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String toString() {
+		return "Circle: radius=" + radius + " color=" + color;
+	}
+}
---
+++ t0e1/src/t0e1/s1/i4/MyCircle.java
@@ -0,0 +1,56 @@
+package t0e1.s1.i4;
+
+import t0e1.s1.i3.MyPoint;
+
+public class MyCircle {
+	private MyPoint center;
+	private int radius;
+
+	public MyCircle(MyPoint center, int radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+
+	public MyCircle(int x, int y, int radius) {
+		this.center= new MyPoint(x,y);
+		this.radius = radius;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public MyPoint getCenter() {
+		return center;
+	}
+
+	public void setCenter(MyPoint center) {
+		this.center = center;
+	}
+
+	public int getCenterX() {
+		return center.getX();
+	}
+
+	public int getCenterY() {
+		return center.getY();
+	}
+
+	public void setCenterXY(int x, int y) {
+		this.center.setXY(x, y);
+	}
+
+	public String toString() {
+		return ("Circle @ " + this.getCenter().toString() + " radius=" + this.getRadius());
+
+	}
+
+	public double getArea() {
+		return (Math.PI * this.getRadius() * this.getRadius());
+	}
+
+}
