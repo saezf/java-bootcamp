c4148096e1b14cbd0a44da0303c39a26a70e3665
Rodrigo <rodrigocardozo@gmail.com>
2015-02-01 01:43:58 -0300

    fixed topic 5, add files

--- topic5/high_school.sql
+++ topic5/high_school.sql
@@ -2,12 +2,14 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+CREATE SCHEMA IF NOT EXISTS `high_school` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
 CREATE TABLE IF NOT EXISTS `high_school`.`student` (
   `Id` INT(11) NOT NULL AUTO_INCREMENT,
-  `First_name` VARCHAR(45) NOT NULL,
-  `Last_name` VARCHAR(45) NOT NULL,
-  `Registration_numberl` INT(11) NOT NULL,
-  `Birthdate` DATE NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `registration_number` INT(11) NOT NULL,
+  `birthday` DATE NOT NULL,
   PRIMARY KEY (`Id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
@@ -15,9 +17,9 @@ COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `high_school`.`teacher` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `First_name` VARCHAR(45) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
   `Last_name` VARCHAR(45) NOT NULL,
-  `Birthdate` DATE NOT NULL,
+  `birthday` DATE NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
@@ -25,35 +27,12 @@ COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `high_school`.`course` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `teacher_id` INT(11) NULL DEFAULT NULL,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS `high_school`.`students_course` (
-  `students_id` INT(11) NOT NULL,
-  `course_id` INT(11) NOT NULL,
-  `teacher_id` INT(11) NOT NULL,
-  `note1` DOUBLE NOT NULL,
-  `note2` DOUBLE NOT NULL,
-  `note3` DOUBLE NOT NULL,
-  `finalnote` DOUBLE NOT NULL,
-  INDEX `student_id_idx` (`students_id` ASC),
-  INDEX `course_id_idx` (`course_id` ASC),
-  INDEX `fk_students_course_teacher1_idx` (`teacher_id` ASC),
-  PRIMARY KEY (`students_id`, `course_id`),
-  CONSTRAINT `student_id`
-    FOREIGN KEY (`students_id`)
-    REFERENCES `high_school`.`student` (`Id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `course_id`
-    FOREIGN KEY (`course_id`)
-    REFERENCES `high_school`.`course` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_students_course_teacher1`
+  `hours_per_week` DOUBLE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_course_teacher1_idx` (`teacher_id` ASC),
+  CONSTRAINT `fk_course_teacher1`
     FOREIGN KEY (`teacher_id`)
     REFERENCES `high_school`.`teacher` (`id`)
     ON DELETE NO ACTION
@@ -62,30 +41,53 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS `high_school`.` schedule_time` (
+CREATE TABLE IF NOT EXISTS `high_school`.`student_course` (
+  `student_id` INT(11) NOT NULL,
+  `course_id` INT(11) NOT NULL,
+  `note1` DOUBLE NOT NULL,
+  `note2` DOUBLE NOT NULL,
+  `note3` DOUBLE NOT NULL,
+  `finalnote` DOUBLE NOT NULL,
+  INDEX `student_id_idx` (`student_id` ASC),
+  INDEX `course_id_idx` (`course_id` ASC),
+  PRIMARY KEY (`student_id`, `course_id`),
+  CONSTRAINT `student_id`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `high_school`.`student` (`Id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `course_id`
+    FOREIGN KEY (`course_id`)
+    REFERENCES `high_school`.`course` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `high_school`.`schedule` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  ` desc` VARCHAR(45) NOT NULL,
-  `day` DATE NULL DEFAULT NULL,
-  `start_hour` TIME NULL DEFAULT NULL,
-  `finish_hour` TIME NULL DEFAULT NULL,
+  `day` ENUM('Sunday', 'Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday') NOT NULL,
+  `start_hour` TIME NOT NULL,
+  `finish_hour` TIME NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `high_school`.`course_schedule` (
-  `fk_course_id` INT(11) NOT NULL,
+  `course_id1` INT(11) NOT NULL,
   `schedule_id` INT(11) NOT NULL,
-  PRIMARY KEY (`fk_course_id`, `schedule_id`),
+  PRIMARY KEY (`course_id1`, `schedule_id`),
   INDEX `schedule_id_idx` (`schedule_id` ASC),
   CONSTRAINT `fk_course_id`
-    FOREIGN KEY (`fk_course_id`)
+    FOREIGN KEY (`course_id1`)
     REFERENCES `high_school`.`course` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `schedule_id`
     FOREIGN KEY (`schedule_id`)
-    REFERENCES `high_school`.` schedule_time` (`id`)
+    REFERENCES `high_school`.`schedule` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
